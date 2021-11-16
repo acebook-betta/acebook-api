ea3b1c24ea587230503fa8c428393363ee2e4d95
ConorButler <cwdbutler@gmail.com>
2021-11-16 15:07:01 +0000

    Improved database constraints, changing content to text

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :posts
-  validates_uniqueness_of :email
+  
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_142218) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "content"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
